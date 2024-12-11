2620e4ab68ee4dc409982525df6d06ad2a90c191
Andrei Maiboroda <andrei@ethereum.org>
2024-12-11 11:40:58 +0100

    t8n: Collect EIP-7002 and EIP-7251 requests

--- test/t8n/t8n.cpp
+++ test/t8n/t8n.cpp
@@ -224,8 +224,11 @@ int main(int argc, const char* argv[])
             if (rev >= EVMC_PRAGUE)
             {
                 requests.emplace_back(collect_deposit_requests(receipts));
-                requests.emplace_back(state::Requests::Type::withdrawal);
-                requests.emplace_back(state::Requests::Type::consolidation);
+
+                auto system_call_requests =
+                    system_call_block_end(state, block, block_hashes, rev, vm);
+                std::move(system_call_requests.begin(), system_call_requests.end(),
+                    std::back_inserter(requests));
             }
 
             test::finalize(
